a0e4717261189151d6b2ab237a2930466f861d3c
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-06-11 11:47:49 -0600

    textos svg y formas

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const canvas = d3.select(".canvas")
 
 const svg = canvas.append('svg')
 
+svg.attr('height', 600)
+svg.attr('width', 600)
+
 svg.append('rect')
+    .attr('width', 200)
+    .attr('height', 100)
+    .attr('fill', 'blue')
+    .attr('x', 20)
+    .attr('y', 20)
+
 svg.append('circle')
+    .attr('r', 50)
+    .attr('cx', 300)
+    .attr('cy', 70)
+    .attr('fill', 'pink')
+
 svg.append('line')
+    .attr('x1', 370)
+    .attr('y1', 400)
+    .attr('x2', 120)
+    .attr('y2', 20)
+    .attr('stroke', 'red')
+
+svg.append('text')
+    .attr('x',20)
+    .attr('y',200)
+    .attr('fill','gray')
+    .text('Hello world')
+    .style('font-family','arial')
